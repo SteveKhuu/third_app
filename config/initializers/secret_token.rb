c2e5b675cca1fd2799df493fbc7347aaa3c55fce
Stephen Khuu <Stephen_Khuu@epam.com>
2012-10-22 12:00:47 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ThirdApp::Application.config.secret_token = '073cc6a31ee4d877584c3e40644bc5a170f0ed497d213edfafd05a896d0e1766b272726beec1ddd5d92232ef5d290db31167535d2cff9c1563c475c65523b619'
