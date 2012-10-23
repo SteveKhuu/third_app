76b32ce67202e0904e85531a80a03aa75730468e
Stephen Khuu <Stephen_Khuu@epam.com>
2012-10-23 12:02:32 -0400

    Refactored page titles

--- config/application.rb
+++ config/application.rb
@@ -39,6 +39,9 @@ module ThirdApp
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
+    config.colorize_logging = false
+
+    
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
 
